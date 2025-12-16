61a2b60e2f0a93b79663c21f4dcfd22fdff452af
root <haibara98@qq.com>
2025-12-16 11:54:34 +0800

    a

--- make-front-end/run.sh
+++ make-front-end/run.sh
@@ -1,6 +1,9 @@
+rm -rf ../docs/*
+rm -rf /var/www/web/*
+sleep 1
 NODE_OPTIONS=--openssl-legacy-provider npm run build
 sleep 1
 cd ..
-cp -r docs/ /var/www/web
+cp -r docs/* /var/www/web
 sleep 1
 cd make-front-end/
