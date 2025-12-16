03a7a068c918d9a8b9053101b1abfb38255e7a92
root <haibara98@qq.com>
2025-12-16 12:04:08 +0800

    update

--- make-front-end/run.sh
+++ make-front-end/run.sh
@@ -1,9 +1,13 @@
 rm -rf ../docs/*
-rm -rf /var/www/web/*
 sleep 1
 NODE_OPTIONS=--openssl-legacy-provider npm run build
 sleep 1
-cd ..
-cp -r docs/* /var/www/web
+sed -i 's/preview\.pro\.ant\.design/gitasoul.niaohan.top/g' ../docs/CNAME
 sleep 1
-cd make-front-end/
+cd ..
+git add .
+sleep 1
+git commit -m "update"
+sleep 1
+git push
+cd make-front-end
